283a48e33ea9f9208dda29ab8cb1446a1b98a80e
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2021-05-16 21:27:21 +0530

    Create ArrangeTheSheeps.cpp

---
+++ prefix/1-D/ArrangeTheSheeps.cpp
@@ -0,0 +1,5 @@
+status: NOT_SOLVED.
+problem: https://codeforces.com/contest/1520/problem/E
+solutions: https://codeforces.com/submissions/JustNik77/page/2
+           https://codeforces.com/contest/1520/submission/115315773
+similar problem: https://atcoder.jp/contests/abc102/tasks/arc100_a
